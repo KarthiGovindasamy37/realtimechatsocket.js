51b345cb6554b4ac26fa744689b83d3282c17e1b
KarthiGovindasamy37 <107383078+KarthiGovindasamy37@users.noreply.github.com>
2023-04-17 13:01:49 +0530

    Updated

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ const removeUser = (socketId) => {
     users = users.filter(e => e.socketId !== socketId)
 }
 
+const removeUserById = (id) =>{
+    users = users.filter(e => e.userId != id)
+}
+
 io.on("connection",(socket) =>{
     
     socket.on("addUser",(userId) =>{console.log("adduser") 
@@ -26,22 +30,37 @@ io.on("connection",(socket) =>{
         io.emit("usersList",users)
     })
 
-    socket.on("sendMessage",({senderId,receiverId,message}) => {
+    socket.on("sendMessage",({senderId,receiverId,message,createdAt,_id,timestamp}) => {console.log("senddd");
         let receiver = users.find(e => e.userId === receiverId)
-        console.log(message);
-        console.log(receiverId);
-        console.log(senderId);
-        console.log(users);
+        // console.log(message);
+        // console.log(receiverId);
+        // console.log(senderId);
+        // console.log(users);
         if(receiver)io.to(receiver.socketId).emit("getMessage",{
             sender : senderId,
-            createdAt : new Date(),
-            message
+            createdAt,
+            message,
+            _id,
+            timestamp
         })
     })
 
-    socket.on("disconnect",() =>{console.log("diss");
+    socket.on("changeStatus",({newMessageList,friend,userId}) =>{
+        console.log(newMessageList,friend,userId)
+      let receiver = users.find(e => e.userId === friend)
+      console.log(receiver)
+      if(receiver) io.to(receiver.socketId).emit("updateMessageStatus",{friendId:userId,newMessageList}) 
+    })
+
+    socket.on("disconnect",() =>{console.log("diss")
         removeUser(socket.id)
         io.emit("usersList",users)
+    })
+
+    socket.on("removeUser",(id) =>{console.log("logout");
+        removeUserById(id)
+        io.emit("usersList",users)
+        console.log(users);
     })
 })
 
